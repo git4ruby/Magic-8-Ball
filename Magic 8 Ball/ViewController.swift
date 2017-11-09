f8e3e8e7b7d5df47234ff9da321840c7f1b16e11
Mohit Chandana <chandanamohit@gmail.com>
2017-11-08 20:13:11 -0700

    Add icons, images and write random generator code

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 1
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        randomBallNumber = Int(arc4random_uniform(4))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
